59300793227f1bdb93b372f12df3bd394dfc4e45
Prashant Gunjal <prashantgunjal.dev@gmail.com>
2024-03-08 09:09:56 +0530

    Day 1 - React Js

---
+++ vit-hello/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header /> <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vit-hello/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h3>Let's learn React Js</h3>
+    </header>
+  );
+}
+
+export default Header;
