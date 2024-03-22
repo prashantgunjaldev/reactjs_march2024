de7ccfd65e7e03c028336b8b5f00b9f4a9482d5a
Prashant Gunjal <prashantgunjal.dev@gmail.com>
2024-03-22 09:01:53 +0530

    React Js with Scss

---
+++ react-scss/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.scss";
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a> Home Page </a>
+          </li>
+          <li>
+            <a> About Page </a>
+          </li>
+          <li>
+            <a> Contact Page </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
